675f675e0330f956adc811188ecb6fee8774caa9
Matt DeCamp <matthewdecamp@gmail.com>
2020-07-30 21:09:33 -0400

    styling changes for tablet, desktop; eleventy engine; meta and site data added

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = (config) => {
+    // Set directories to pass through to the dist folder
+    config.addPassthroughCopy("./src/js/");
+    return {
+        markdownTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
+        dir: {
+            input: "src",
+            output: "dist",
+        },
+    };
+};
